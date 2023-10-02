23061abe52d31c0568f7ffc8542d2b939cbecc85
itsyouyouAgain <qing.yo@northeastern.edu>
2023-10-02 18:52:55 -0400

    commiting food

---
+++ TableViewControllerFood/TableViewControllerFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewControllerFood
+//
+//  Created by Youying Qing on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
